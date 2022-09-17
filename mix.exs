932dfb4826e9240b66a6e7aa2f1ec1d509f6c329
Kabei <kambei.sapote@gmail.com>
2022-09-17 00:40:08 -0400

    update

--- mix.exs
+++ mix.exs
@@ -23,7 +23,6 @@ defmodule Ipnutils.MixProject do
     [
       {:decimal, "~> 2.0"},
       {:jason, "~> 1.3"},
-      # {:cowlib, "~> 2.11"},
       {:ecto_sql, "~> 3.7"},
       {:cubdb, git: "https://github.com/lucaong/cubdb"},
       {:benchee, "~> 1.0", only: :dev}
